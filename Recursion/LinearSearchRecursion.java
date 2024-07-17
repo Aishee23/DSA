66442654267e66b54512fda212939bf8b676117e
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-07-17 21:19:31 +0530

    Update LinearSearchRecursion.java

--- Recursion/LinearSearchRecursion.java
+++ Recursion/LinearSearchRecursion.java
@@ -3,7 +3,10 @@ public class LinearSearchRecursion {
     {
         int[] arr={1,90,4,59,99};
         System.out.println(finds(arr,99,0));
-        System.out.println(find(arr,99,0));
+        //System.out.println(find(arr,99,0));
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> ans= findAllIndex(arr,99,0,list);
+        System.out.println(ans);
     }
     //function for true and
     static boolean finds(int[] arr,int target,int index)
@@ -27,4 +30,13 @@ public class LinearSearchRecursion {
           }else 
           return  find(arr,target,index+1);
     }
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+    if (index == arr.length) {
+        return list;
+    }
+    if (arr[index] == target) {
+        list.add(index);
+    }
+    return findAllIndex(arr, target, index + 1, list);
+}
 }
