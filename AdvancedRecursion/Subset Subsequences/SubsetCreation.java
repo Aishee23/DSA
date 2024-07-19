00a28fa49ad085297617453b3f87c7cc74a4a450
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-07-19 19:42:55 +0530

    Update SubsetCreation.java

--- AdvancedRecursion/Subset Subsequences/SubsetCreation.java
+++ AdvancedRecursion/Subset Subsequences/SubsetCreation.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class SubsetCreation {
     public static void main(String[] args){
         subsetcreate("","abc");
+        //store as arraylist
+        System.out.println(subsetcreat("","abc"));
     }
     static void subsetcreate(String p, String up)
     {
@@ -13,5 +17,19 @@ public class SubsetCreation {
         subsetcreate(p+ch,up.substring(1));
         subsetcreate(p,up.substring(1));
       
+    }
+    static ArrayList<String> subsetcreat(String p, String up)
+    {
+        if(up.isEmpty())
+        {   ArrayList<String> list = new ArrayList<String>();
+            list.add(p);
+            //System.out.println(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> left=subsetcreat(p+ch,up.substring(1));
+        ArrayList<String> right =subsetcreat(p,up.substring(1));
+        left.addAll(right); //add the left and right elements of the recursive tree
+        return left;
     }
 }
