78166249565bd4524e0673288dca431b1e522893
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-07-20 18:21:53 +0530

    Update PhonePadCombinations.java

--- AdvancedRecursion/permutations/PhonePadCombinations.java
+++ AdvancedRecursion/permutations/PhonePadCombinations.java
@@ -1,6 +1,6 @@
 
 //INPUT: 12 in Old Phone Pad
-//OUTPUT: [ad, ae, af, bd, be, bf, cd, ce, cf]
+//OUTPUT: [ad, ae, af, bd, be, bf, cd, ce, cf]   also counts the number of permutations using the padcount funcction
 
 import java.util.ArrayList;
 public class PhonePad {
@@ -42,6 +42,22 @@ public class PhonePad {
             ans.addAll(padret(p + ch, up.substring(1)));
         }
         return ans;
+    }
+static int padcount(String p, String up)
+    {
+        if (up.isEmpty()) 
+        {
+            System.out.println(p);
+            return 1;
+        }
+        int count =0;
+        int digit= up.charAt(0)-'0';     //this will convert '2' to 2
+        for (int i=(digit-1)*3;i<digit*3;i++) 
+        {
+            char ch = (char) ('a' + i); //start from 'a' and continue 
+            count =count+ padcount(p + ch, up.substring(1));
+        }
+        return count;
     }
 }
 
