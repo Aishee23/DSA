4ffb0b2c1eaece50dae4d1a535bb4113345e80c8
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-07-20 18:00:08 +0530

    Create PhonePadCombinations.java

---
+++ AdvancedRecursion/permutations/PhonePadCombinations.java
@@ -0,0 +1,53 @@
+
+//INPUT: 12 in Old Phone Pad
+//OUTPUT: [ad, ae, af, bd, be, bf, cd, ce, cf]
+
+import java.util.ArrayList;
+public class PhonePad {
+    public static void main(String[] args)
+    {
+     //pad("","12");
+     ArrayList<String> ans = padret("", "12");
+    System.out.println(ans);
+    }
+    static void pad(String p, String up)
+    {
+        if (up.isEmpty()) 
+        {
+            System.out.println(p);
+            return;
+        }
+        int digit= up.charAt(0)-'0';     //this will convert '2' to 2
+        for (int i=(digit-1)*3;i<digit*3;i++) 
+        {
+            char ch = (char) ('a' + i); //start from 'a' and continue 
+            pad(p + ch, up.substring(1));
+        }
+    }
+
+    static ArrayList<String> padret(String p, String up)
+    {
+        if (up.isEmpty()) 
+        {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(p);
+            return list;
+
+        }
+        ArrayList<String> ans = new ArrayList<String>();
+        int digit= up.charAt(0)-'0';     //this will convert '2' to 2
+        for (int i=(digit-1)*3;i<digit*3;i++) 
+        {
+            char ch = (char) ('a' + i); //start from 'a' and continue 
+            ans.addAll(padret(p + ch, up.substring(1)));
+        }
+        return ans;
+    }
+}
+
+
+
+
+//INPUT: 12
+//OUTPUT: [ad, ae, af, bd, be, bf, cd, ce, cf]
+
