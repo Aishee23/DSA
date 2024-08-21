96af2915e3b215039e851980f3bc591cb01649fa
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-08-21 13:51:31 +0530

    Update DoublyLL.java

--- Linked List/DoublyLL.java
+++ Linked List/DoublyLL.java
@@ -46,6 +46,36 @@ public class DoublyLL {
         
     }
 
+    //insert at any position after a node
+        public Node find(int val) {
+            Node node = head;
+            
+            while(node != null) {
+                if(node.val ==val)
+                {
+                    return node;
+                }
+                node=node.next;
+        }
+        return null;
+        }   
+    public void insert(int after, int val) {
+            Node p = find(after);
+    
+            if (p == null) {
+                System.out.println("does not exist");
+                return;
+            }
+    
+            Node node = new Node(val);
+            node.next = p.next;
+            p.next = node;
+            node.prev = p;
+            if (node.next != null) {
+                node.next.prev = node;
+            }
+    }
+
     //Display function
     public void display()
     {
