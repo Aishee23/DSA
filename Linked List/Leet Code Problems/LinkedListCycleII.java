5a0b8c8b7775a5df874cacba403227ad3336d310
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-08-22 23:33:27 +0530

    Update LinkedListCycleII.java

--- Linked List/Leet Code Problems/LinkedListCycleII.java
+++ Linked List/Leet Code Problems/LinkedListCycleII.java
@@ -1,6 +1,11 @@
 
 //https://leetcode.com/problems/linked-list-cycle-ii/description/
-
+//HOW TO APPROACH THE PROBLEM
+/*1. We use two pointers, slow and fast, to traverse the linked list. slow moves one step at a time, while fast moves two steps at a time.
+2.If there is a cycle, fast will eventually catch up to slow within the cycle.
+3.Once slow and fast meet, we reset slow to the head of the list and keep fast at the meeting point.
+4.We then move both slow and fast one step at a time. When they meet again, it will be at the starting point of the cycle.
+*/
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         if (head == null) {
