181bf24e02f06adab8996a36252a1d6a87d5f535
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-08-22 13:41:07 +0530

    Update Delete_Duplicates_from_sortedList.java

--- Linked List/Leet Code Problems/Delete_Duplicates_from_sortedList.java
+++ Linked List/Leet Code Problems/Delete_Duplicates_from_sortedList.java
@@ -11,17 +11,18 @@
  
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode node=head;
-        while(node.next!=null){
-            if(node.val== node.next.val)
-            {
-                node.next=node.next.next;
-                
-            }else{
-                node=node.next;
+        if (head == null) {
+            return head;
+        }
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
             }
-            
         }
         return head;
     }
+}
 }
