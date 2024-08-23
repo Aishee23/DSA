836d7c43c569a678994749b3ac744b3ffecd03f9
Aishee Dutta <111168865+Aishee23@users.noreply.github.com>
2024-08-24 00:04:10 +0530

    Create InPlaceReversal.java

---
+++ Linked List/Leet Code Problems/InPlaceReversal.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/reverse-linked-list/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null)
+        {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode pres = head;
+        ListNode nex = pres.next;
+        while(pres!=null)
+        {
+            pres.next=prev;
+            prev=pres;
+            pres=nex;
+            if(nex!=null)
+            {
+                nex=nex.next;
+            }
+        }
+        
+        return prev;
+    }
+}
